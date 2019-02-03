4503a39ee2cb5d3e8b68251c4e3a9df4095e9874
vklimov <vklimov@livemaster.ru>
2019-02-03 22:47:57 +0300

    started html/css

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../scss/index.scss';
 import sum from './sum';
 
-const title = document.getElementById('title');
-title.innerText = sum(4, 6, 8, 7, 19, 888).toString();
+// const title = document.getElementById('title');
+// title.innerText = sum(4, 6, 8, 7, 19, 888).toString();
