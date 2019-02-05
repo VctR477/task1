dbce2669057dc1dfd948dd57df4b4dc843cc1a0b
vklimov <vklimov@livemaster.ru>
2019-02-05 23:44:59 +0300

    remake code

--- src/js/app.js
+++ src/js/app.js
@@ -1,114 +1,137 @@
 /**
  * TODO
+ * 1. список меняется при измении карты
  * 2. маркер без надписи
  * 3. другой балун
  * 4. перетаскивание списка
- * 5. список меняется при измении карты
- * 6. рефакторинг
- * 7. тесты
- * 8. реакт
- * 9. тесты для реакт
+ * 5. рефакторинг
+ * 6. тесты
+ * 7. реакт
+ * 8. тесты для реакт
  */
-
-
-
-// import { createNewPoint, createRoute } from './modules';
 
 const KEY_CODE_ENTER = 13;
 const POINTS_LIST_ELEM = document.getElementById('address-list');
 const ADDRESS_INPUT_ELEM = document.getElementById('add-new-point');
 
-const _createNewItemOfAddressList = (address) => {
-	const removeBtnTemplate = `
-		<div class="address-list__item">
-			<span class="address-list__point-name">${address}</span>
-			<button class="address-list__remove-btn" type="button" data-address="${address}">X</button>
-		</div>
-	`;
-	POINTS_LIST_ELEM.insertAdjacentHTML('beforeend', removeBtnTemplate);
-};
+class RouteMap {
+	constructor() {
+		this.myMap = null;
+		this.multiRoute = null;
+		this.suggestView = null;
+		this.suggestItemSelected = false;
+		this.routePointsArr = [];
+		this.init();
+	}
 
-let isSelect = false;
-let referencePoints = [];
+	init() {
+		this.createMap();
+		this.createMultiRoute();
+		this.createSuggestView();
+		this.bindEvents();
+	}
 
-const app = () => { 
-	// Создание карты.    
-	const myMap = new ymaps.Map('map', {
-		// Координаты центра карты.
-		// Порядок по умолчанию: «широта, долгота».
-		// Чтобы не определять координаты центра карты вручную,
-		// воспользуйтесь инструментом Определение координат.
-		center: [55.76, 37.64],
-		// Уровень масштабирования. Допустимые значения:
-		// от 0 (весь мир) до 19.
-		zoom: 7,
-		controls: [ 'zoomControl']
-	});
+	createMap() {
+		// Создание карты.
+		this.myMap = new ymaps.Map('map', {
+			// Координаты центра карты.
+			center: [55.76, 37.64],
+			// Уровень масштабирования. Допустимые значения:
+			// от 0 (весь мир) до 19.
+			zoom: 7,
+			controls: [ 'zoomControl']
+		});
+	}
 
-	const multiRoute = new ymaps.multiRouter.MultiRoute(
-		{
-			referencePoints: referencePoints
-		},
-		{
-			boundsAutoApply: true,
-			// balloonLayout: myBalloonLayout,
-			balloonPanelMaxMapArea: 0
-	
-		}
-	);
-	// Включение режима редактирования.
-	multiRoute.editor.start();
-	
-	// Добавление маршрута на карту.
-	myMap.geoObjects.add(multiRoute);
+	createMultiRoute() {
+		this.multiRoute = new ymaps.multiRouter.MultiRoute(
+			{
+				referencePoints: []
+			},
+			{
+				boundsAutoApply: true,
+				// balloonLayout: myBalloonLayout,
+				balloonPanelMaxMapArea: 0
+		
+			}
+		);
+		// Включение режима редактирования.
+		this.multiRoute.editor.start();
+		
+		// Добавление маршрута на карту.
+		this.myMap.geoObjects.add(this.multiRoute);
+	}
 
-	// подключаем попап с подсказками адресов для нашего инпута
-	const suggestView = new ymaps.SuggestView('add-new-point');
+	createSuggestView() {
+		this.suggestView = new ymaps.SuggestView('add-new-point');
+	}
 
-	const addNewPoint = (address) => {
-		referencePoints.push(address);
-		multiRoute.model.setReferencePoints(referencePoints);
-		_createNewItemOfAddressList(address);
+	/**
+	 * 
+	 * @param {string} address - адрес новой точки
+	 */
+	addNewPoint(address) {
+		// добавляем новый адрес в наш список
+		this.routePointsArr.push(address);
+		// рисуем наш маршрут заново с новым список
+		this.multiRoute.model.setReferencePoints(this.routePointsArr);
+		// добавляем адрес в список на странице
+		this.createNewItemOfAddressList(address);
 		ADDRESS_INPUT_ELEM.value = '';
 
 		// вешаем обработчик собития на удаление
 		document.querySelector(`.address-list__remove-btn[data-address="${address}"]`).addEventListener('click', event => {
 			const target = event.currentTarget;
 			const address = target.getAttribute('data-address');
-			referencePoints = referencePoints.filter(item => item !== address);
+			this.routePointsArr = this.routePointsArr.filter(item => item !== address);
 			POINTS_LIST_ELEM.removeChild(target.parentNode);
-			multiRoute.model.setReferencePoints(referencePoints);
+			this.multiRoute.model.setReferencePoints(this.routePointsArr);
 		});
-	};
-	// вешаем обработчик события на выбор адреса
-	suggestView.events.add('select', event => {
-		isSelect = true;
-		const address = event.get('item').value;
-		addNewPoint(address);
-	});
+	}
 
-	// на случай если пользователь ничего не выбрал, а просто нажал интер
-	ADDRESS_INPUT_ELEM.addEventListener('keypress', event => {
-		const val = event.currentTarget.value;
-		if (event.keyCode !== KEY_CODE_ENTER || !val) return;
-		if (isSelect) {
-			isSelect = false;
-			return;
-		};
-		addNewPoint(suggestView.state.get('items')[0].value);
-	});
+	createNewItemOfAddressList(address) {
+		const removeBtnTemplate = `
+			<div class="address-list__item">
+				<span class="address-list__point-name">${address}</span>
+				<button class="address-list__remove-btn" type="button" data-address="${address}">X</button>
+			</div>
+		`;
+		POINTS_LIST_ELEM.insertAdjacentHTML('beforeend', removeBtnTemplate);
+	}
 
-	multiRoute.events.add('boundschange', function () {
-		if (referencePoints.length) {
-			myMap.setBounds(multiRoute.getBounds(), {
-				checkZoomRange: true
-			});
-		} else {
-			myMap.setCenter([55.76, 37.64], 7, {
-				checkZoomRange: true
-			});
-		}
-	});
-};
+	bindEvents() {
+		// вешаем обработчик события на выбор адреса
+		this.suggestView.events.add('select', event => {
+			this.suggestItemSelected = true;
+			const address = event.get('item').value;
+			this.addNewPoint(address);
+		});
+
+		// на случай если пользователь ничего не выбрал, а просто нажал интер
+		ADDRESS_INPUT_ELEM.addEventListener('keypress', event => {
+			const val = event.currentTarget.value;
+			if (event.keyCode !== KEY_CODE_ENTER || !val || this.suggestItemSelected) {
+				isSelect = false;
+				return;
+			}
+			const address = this.suggestView.state.get('items')[ 0 ].value;
+			this.addNewPoint(address);
+		});
+
+		this.multiRoute.events.add('boundschange', () => {
+			if (this.routePointsArr.length) {
+				this.myMap.setBounds(this.multiRoute.getBounds(), {
+					checkZoomRange: true
+				});
+			} else {
+				this.myMap.setCenter([55.76, 37.64], 7, {
+					checkZoomRange: true
+				});
+			}
+		});
+	}
+}
+
+const app = () => new RouteMap();
 
 export default app;
